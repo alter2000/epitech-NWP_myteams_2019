0fb8416f97ae6f3374fab899c4cf086cb47d1f5a
Jor Bashllari <altnate2000@gmail.com>
2020-05-30 01:24:32 +0200

    try_init_client up(ish)

--- client/main.c
+++ client/main.c
@@ -9,8 +9,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include "helpers.h"
 #include "types.h"
+
+void try_init_client(client_t *c, const char *ip)
+{
+    struct sockaddr_in ss = {.sin_addr.s_addr = inet_addr(ip),
+        .sin_port = c->res.port};
+    socklen_t tmp = sizeof(ss);
+
+    c->res.lsn.status = SOCKET_NOT_READY;
+    c->res.lsn.fd = socket(AF_INET, SOCK_STREAM, c->res.p_ent->p_proto);
+    if (c->res.lsn.fd == -1
+        || getsockname(c->res.lsn.fd, (struct sockaddr *)&ss, &tmp) == -1) {
+        close(c->res.lsn.fd);
+        errb(strerror(errno));
+    }
+    c->addr_from = strdup(inet_ntoa(ss.sin_addr));
+    c->addr_to = strdup(ip);
+    c->user = NULL;
+    c->pw = NULL;
+    c->isauth = false;
+    c->res.sin.sin_port = htons(c->res.port);
+    c->res.sin.sin_family = AF_INET;
+    c->res.sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    append_log(c, "client up\n");
+}
+
+void run_client(client_t *c)
+{
+}
 
 int main(int c, char **v)
 {
@@ -21,10 +52,8 @@ int main(int c, char **v)
         show_help(84);
     if (!strncmp("-help", v[1], 5))
         show_help(0);
-    p = strtol(v[1], NULL, 10);
-    cl->ip = (p > 0 && p < 65535) ? p : (long)errb("Invalid port num");
     p = strtol(v[2], NULL, 10);
-    cl->port = (p > 0 && p < 65535) ? p : (long)errb("Invalid port num");
-    try_init_client(cl);
+    cl->res.port = (p > 0 && p < 65535) ? p : (long)errb("Invalid port num");
+    try_init_client(cl, v[1]);
     run_client(cl);
 }
