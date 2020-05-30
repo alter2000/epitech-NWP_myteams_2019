e771b6d0fd0c59071732d5017ac9f0458fa18a61
besart <besart.cerri@epitech.eu>
2020-05-30 14:59:16 +0200

    Commands samples added.

--- server/cmds/cmds.c
+++ server/cmds/cmds.c
@@ -19,6 +19,19 @@
 
 static const cmdpair_t CMDS[] = {
     { NULL   , 0 , cmd_unknown } ,
+    { NULL   , false , 0 , cmd_unknown } ,
+    { NULL   , false , 0 , cmd_help } ,
+    { NULL   , false , 0 , cmd_login } ,
+    { NULL   , false , 0 , cmd_logout } ,
+    { NULL   , false , 0 , cmd_user } ,
+    { NULL   , false , 0 , cmd_users } ,
+    { NULL   , false , 0 , cmd_send } ,
+    { NULL   , false , 0 , cmd_messages } ,
+    { NULL   , false , 0 , cmd_subscribe } ,
+    { NULL   , false , 0 , cmd_unsubscribe } ,
+    { NULL   , false , 0 , cmd_create } ,
+    { NULL   , false , 0 , cmd_list } ,
+    { NULL   , false , 0 , cmd_info } ,
 };
 
 static cmdstr_t *getcmd(char *buf)
@@ -53,5 +66,77 @@ void cmd_unknown(client_t *c, char *buf)
 {
     if (buf)
         memset(buf, 0, strlen(buf));
-    msgsend(c->res.lsn.fd, 500, "");
+    msgsend(c->res.lsn.fd, 500, "This is just unknown");
+}
+void cmd_help(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is help_cmd");
+}
+void cmd_login(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is login_cmd");
+}
+void cmd_logout(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is logout_cmd");
+}
+void cmd_user(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is user_cmd");
+}
+void cmd_users(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is users_cmd");
+}
+void cmd_send(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is send_cmd");
+}
+void cmd_messages(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is messages_cmd");
+}
+void cmd_subscribe(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is subscribe_cmd");
+}
+void cmd_unsubscribe(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is unsubscribe_cmd");
+}
+void cmd_create(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is create_cmd");
+}
+void cmd_list(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is list_cmd");
+}
+void cmd_info(client_t *c, char *buf)
+{
+    if (buf)
+        memset(buf, 0, strlen(buf));
+    msgsend(c->res.lsn.fd, 500, "This is info_cmd");
 }
