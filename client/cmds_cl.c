d1b5fec61e43d85c7e69219a57c95daf49f36d7d
Jor Bashllari <altnate2000@gmail.com>
2020-05-31 21:22:52 +0200

    fix warnings

--- client/cmds_cl.c
+++ client/cmds_cl.c
@@ -14,24 +14,33 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include "../include/cmds.h"
-#include "../include/client.h"
+#include "cmds.h"
+#include "client.h"
+#include "myteams/logging_client.h"
+
+static int cl_cmd_unknown(const char *a, const char *b)
+{
+    (void)b;
+    printf("%s is not a valid command\n", a);
+    return 0;
+}
 
 static const cl_cmdpair_t CMDS_CL[] = {
-    { "/help"        , 5  , cmd_help        } ,
-    { NULL           , 0  , cmd_unknown     } ,
+    { "/login"  , 6 , client_event_loggedin   } ,
+    { "/logout" , 7 , client_event_loggedout } ,
+    { NULL      , 0 , cl_cmd_unknown     } ,
 };
 
 void getcmd_cl(client_t *c, char *buf)
 {
-    cl_cmdptr fn = cmd_unknown;
+    cl_cmdptr fn = cl_cmd_unknown;
     char *cmd = strtok(buf, " ");
 
     if (!cmd)
-        fn(c->uuid, c->user);
+        fn((const char*)c->uuid, c->user);
     for (int i = 0; CMDS_CL[i].s; i++)
         if (!strncasecmp(CMDS_CL[i].s, cmd, CMDS_CL[i].slen)) {
-            CMDS_CL[i].cmd(c->uuid, c->user);
+            CMDS_CL[i].cmd((const char*)c->uuid, c->user);
             return;
         }
 }
